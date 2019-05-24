d6a0f7fd1f8bf688a55b6d518f33b97ff2b370df
Rhys Bowen Harries <rbharries@gmail.com>
2019-05-24 11:17:24 +0100

    data showing on log

---
+++ client/src/views/entry_view.js
@@ -0,0 +1,18 @@
+
+
+
+const EntryView = function (element) {
+  this.element = element
+}
+
+EntryView.prototype.render = function (data) {
+  console.log(data);
+}
+
+
+
+
+
+
+
+module.exports = EntryView;
--- client/src/views/list_view.js
+++ client/src/views/list_view.js
@@ -1,4 +1,5 @@
 const PubSub = require('../helpers/pub_sub.js');
+const EntryView = require('./entry_view.js')
 
 const ListView = function (element) {
   this.element = element
@@ -7,11 +8,17 @@ const ListView = function (element) {
 ListView.prototype.bindEvents = function () {
   PubSub.subscribe("Model:all-data", (event) => {
     const allData = event.detail
-    console.log(allData);
+    this.populate(allData)
   })
 };
 
+ListView.prototype.populate = function (allData) {
+  allData.forEach( (data) => {
 
+    const tile = new EntryView(this.element)
+    tile.render(data)
+  })
+};
 
 
 
