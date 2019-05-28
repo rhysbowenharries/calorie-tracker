0081ec08c36b91be3fd65167de798c3003c8cddc
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-05-28 16:47:09 +0100

    form working again

--- client/src/views/entry_view.js
+++ client/src/views/entry_view.js
@@ -7,37 +7,36 @@ const EntryView = function (element) {
 
 EntryView.prototype.render = function (data, index) {
 
-  const tile = document.createElement('tr')
+  const tile = document.createElement('form')
+  tile.classList.add('update-form')
   tile.id = index
 
+  this.createLabel('Name: ', tile);
   this.createOurElement(data.foodName, `nameElement${index}`, tile)
+  this.createLabel('Calories: ', tile);
   this.createOurElement(data.calories, `caloriesElement${index}`, tile)
+  this.createLabel('Date: ', tile);
   this.createOurElement(data.date, `dateElement${index}`, tile)
 
-  const buttonD = document.createElement('td');
-  buttonD.id = `delete${index}`
   const deleteButton = document.createElement('button')
   deleteButton.value = data._id
   deleteButton.id = index;
-  this.makeDeleteButton(deleteButton)
-  buttonD.appendChild(deleteButton);
-  tile.appendChild(buttonD)
+  deleteButton.textContent = 'Delete'
+  deleteButton.classList.add(`delete${index}`)
+  tile.appendChild(deleteButton)
   deleteButton.addEventListener('click', (event) => {
     PubSub.publish('EntryView:delete', event)
   })
 
-  const buttonU = document.createElement('td');
-  buttonU.id = `update${index}`
   const updateButton = document.createElement('button')
   updateButton.value = data._id
-  updateButton.id = index;
   updateButton.textContent = 'Update'
-  updateButton.classList.add('update')
-  buttonU.appendChild(updateButton);
-  tile.appendChild(buttonU)
+  updateButton.classList.add(`update${index}`)
+  tile.appendChild(updateButton)
   updateButton.addEventListener('click', (event) => {
+    event.preventDefault();
     const updateForm = new UpdateFormView(this.element, tile);
-    updateForm.renderUpdateForm(data, event.target.id);
+    updateForm.renderUpdateForm(data);
   })
   this.element.appendChild(tile)
 }
@@ -48,17 +47,16 @@ EntryView.prototype.makeDeleteButton = function (deleteButton) {
   // deleteButton.style.display = "inline";
 }
 
-// EntryView.prototype.createLabel = function(name, tile){
-//   const label = document.createElement('label');
-//   label.textContent = name;
-//   tile.appendChild(label);
-// }
+EntryView.prototype.createLabel = function(name, tile){
+  const label = document.createElement('label');
+  label.textContent = name;
+  tile.appendChild(label);
+}
 
 EntryView.prototype.createOurElement = function(input, classList, tile){
-  const name = document.createElement('td')
+  const name = document.createElement('h5')
   name.textContent = input
   name.classList.add(classList);
-  // name.style.display = "inline"
   tile.appendChild(name)
 }
 
--- client/src/views/update_form_view.js
+++ client/src/views/update_form_view.js
@@ -5,58 +5,19 @@ const UpdateFormView = function (element, tile) {
     this.tile = tile;
 }
 
-UpdateFormView.prototype.renderUpdateForm = function (data, updateButtonId) {
+UpdateFormView.prototype.renderUpdateForm = function (data) {
     const objectId = data;
+    console.log('data', data);
+    console.log(this.tile.id)
 
-    const replacementTD = document.querySelector(`.nameElement${updateButtonId}`)
+
+    this.replaceElement(`.nameElement${this.tile.id}`, data.foodName, 'foodName', "text")
+    this.replaceElement(`.caloriesElement${this.tile.id}`, data.calories, "calories", "float")
+    this.replaceElement(`.dateElement${this.tile.id}`, data.date, "date", "date")
+    this.replaceElement(`.delete${this.tile.id}`, "Submit", data._id, "submit")
     
-    const newTD = document.createElement('td');
-    newTD.classList.add('updateForm');
-
-    const form = document.createElement('form');
-    
-    const nameInput = document.createElement('input');
-    nameInput.type = 'text';
-    nameInput.id = 'nameInput'
-    nameInput.name = "foodName"
-    nameInput.value = data.foodName;
-    const caloriesInput = document.createElement('input');
-    caloriesInput.type = 'float';
-    caloriesInput.id = 'caloriesInput'
-    caloriesInput.name = "calories"
-    caloriesInput.value = data.calories;
-    const dateInput = document.createElement('input');
-    dateInput.type = 'date';
-    dateInput.id = 'dateInput'
-    dateInput.name = "date";
-    dateInput.value = data.date;
-
-
-    form.appendChild(nameInput);
-    form.appendChild(caloriesInput);
-    form.appendChild(dateInput);
-    
-
-    const submitButton = document.createElement('input')
-    submitButton.type = "submit"
-    form.appendChild(submitButton);
-
-    newTD.appendChild(form)
-
-    replacementTD.replaceWith(newTD)
-
-    const emptyCell1 = document.createElement('td')
-    const emptyCell2 = document.createElement('td')
-    const emptyCell3 = document.createElement('td')
-    const emptyCell4 = document.createElement('td')
-    const removeCalories = document.querySelector(`td.caloriesElement${updateButtonId}`)
-    removeCalories.replaceWith(emptyCell1)
-    const removeDate = document.querySelector(`td.dateElement${updateButtonId}`)
-    removeDate.replaceWith(emptyCell2);
-    const removeDelete = document.querySelector(`td#delete${updateButtonId}`)
-    removeDelete.replaceWith(emptyCell3);
-    const removeUpdate = document.querySelector(`td#update${updateButtonId}`)
-    removeUpdate.replaceWith(emptyCell4);
+    const removeButton = document.querySelector(`.update${this.tile.id}`);
+    removeButton.parentNode.removeChild(removeButton);
 
     this.tile.addEventListener('submit', (event) => {
         event.preventDefault()
@@ -66,8 +27,6 @@ UpdateFormView.prototype.renderUpdateForm = function (data, updateButtonId) {
 }
 
 UpdateFormView.prototype.getData = function (event, objectId) {
-    console.log(event);
-    
     const foodEntry = {
         _id: objectId._id,
         foodName: event.foodName.value,
@@ -75,15 +34,17 @@ UpdateFormView.prototype.getData = function (event, objectId) {
         date: event.date.value
     }
       return foodEntry
-  };
+};
 
-  UpdateFormView.prototype.replaceElement = function(element, value, id, type){
+UpdateFormView.prototype.replaceElement = function(element, value, id, type){
     const oldName = document.querySelector(element)
     const nameInput = document.createElement('input')
     nameInput.type = type;
     nameInput.id = id;
     nameInput.value = value;
+    nameInput.classList.add(id);
     oldName.replaceWith(nameInput);
-  }
+}
+
 
 module.exports = UpdateFormView;
